53335d1d240e6b0b969c4cecf5ec5cd8ed85670f
Selton Guedes <sfgs@ecomp.poli.br>
2019-03-28 20:51:30 -0300

    Criando pasta static

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-def teste(request):
-    return render(request, 'teste.html', {})
-
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {
+		
+	})
 
