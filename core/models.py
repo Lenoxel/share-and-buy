f4fc28400a836d505b7fa41b38d7778f5ef800a1
Gabriel Lenon <glenonsilva@gmail.com>
2019-04-05 10:36:40 -0300

    modifying urls.py and create new models in core

--- core/models.py
+++ core/models.py
@@ -1,19 +1,71 @@
 from django.db import models
-from django.utils import timezone
+from enum import Enum
 
+class ProductType(Enum): 
+    DIGITAL = "Digital"
+    FISICO = "Físico"
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+class ProductFileFormat(Enum):  
+    PDF = "PDF"
+    MP4 = "MP4"
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class Product(models.Model):
+    produtorId = models.ForeignKey('core.Manufacturer', verbose_name='Produtor', on_delete=models.CASCADE)
+    slug = models.SlugField('Identificador', max_length=100)
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    disponivelEstoque = models.BooleanField("Disponível no estoque", default=False)
+    quantidadeEstoque = models.IntegerField("Quantidade no estoque")
+    afiliadoComissao = models.FloatField("Comissão do afiliado", decimal_places=2, max_digits=5)
+    desconto = models.FloatField("Desconto", decimal_places=2, max_digits=5)
+    emPromocao = models.BooleanField("Em promoção", default=False)
+    tipo = models.CharField('Tipo', max_length=5, choices=[(tag, tag.value) for tag in ProductType])
+    tamanho = models.FloatField("Tamanho [MB]", decimal_places=2, max_digits=15)
+    formato = models.CharField('Formato', max_length=5, choices=[(tag, tag.value) for tag in ProductFileFormat])
+    dataCriacao = models.DateTimeField('Criado em', auto_now_add=True)
+    dataModificacao = models.DateTimeField('Última modificação', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ['nome']
 
     def __str__(self):
-        return self.title
+        return self.name
+
+class Manufacturer(models.Model):
+    slug = models.SlugField('Identificador', max_length=100)
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', max_length=40, unique= True)
+    senha = models.CharField('Senha', max_length=30)
+    produtorPrincipal = models.BooleanField("Produtor Principal", default=False)
+
+    dataCriacao = models.DateTimeField('Criado em', auto_now_add=True)
+    dataModificacao = models.DateTimeField('Última modificação', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Produtor'
+        verbose_name_plural = 'Produtores'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.name
+
+
+class Affiliated(models.Model):
+    slug = models.SlugField('Identificador', max_length=100)
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', max_length=40, unique= True)
+    senha = models.CharField('Senha', max_length=30)
+
+    dataCriacao = models.DateTimeField('Criado em', auto_now_add=True)
+    dataModificacao = models.DateTimeField('Última modificação', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Afiliado'
+        verbose_name_plural = 'Afiliados'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.name
+
--- sharenbuy/urls.py
+++ sharenbuy/urls.py
@@ -13,15 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.conf.urls import url
-
-from core import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-    url(r'^sobre$', views.sobre, name='sobre'),
-    url(r'^planos$', views.planos, name='planos'),
-    url(r'^suporte$', views.suporte, name='suporte'),
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
