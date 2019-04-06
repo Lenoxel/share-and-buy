125cd7f53a8dea08090883700c95e117d7329a90
Gabriel Lenon <glenonsilva@gmail.com>
2019-04-06 16:47:01 -0300

    modificacoes em models e settings

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from enum import Enum
+from phone_field import PhoneField
+from localflavor.br.forms import BRCPFField, BRCNPJField, BRZipCodeField
 
 class ProductType(Enum): 
     DIGITAL = "Digital"
@@ -8,6 +10,35 @@ class ProductType(Enum):
 class ProductFileFormat(Enum):  
     PDF = "PDF"
     MP4 = "MP4"
+
+class BRStates(Enum):
+   AC = "Acre"
+   AL = "Alagoas"
+   AP = "Amapá"
+   AM = "Amazonas"
+   BA = "Bahia"
+   CE = "Ceará"
+   DF = "Distrito Federal"
+   ES = "Espírito Santo"
+   GO = "Goiás"
+   MA = "Maranhão"
+   MT = "Mato Grosso"
+   MS = "Mato Grosso do Sul"
+   MG = "Minas Gerais"
+   PA = "Pará"
+   PB = "Paraíba"
+   PR = "Paraná"
+   PE = "Pernambuco"
+   PI = "Piauí"
+   RJ = "Rio de Janeiro"
+   RN = "Rio Grande do Norte"
+   RS = "Rio Grande do Sul"
+   RO = "Rondônia"
+   RR = "Roraima"
+   SC = "Santa Catarina"
+   SP = "São Paulo"
+   SE = "Sergipe"
+   TO = "Tocantins"
 
 class Product(models.Model):
     produtorId = models.ForeignKey('core.Manufacturer', verbose_name='Produtor', on_delete=models.CASCADE)
@@ -36,9 +67,20 @@ class Product(models.Model):
 class Manufacturer(models.Model):
     slug = models.SlugField('Identificador', max_length=100)
     nome = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, default='')
+    dataNascimento = models.DateField('Data de Nascimento', default='')
     email = models.EmailField('E-mail', max_length=40, unique= True)
+    emailConfirmado = models.BooleanField('E-mail confirmado', default=False)
     senha = models.CharField('Senha', max_length=30)
     produtorPrincipal = models.BooleanField("Produtor Principal", default=False)
+    telefone = PhoneField('Telefone', help_text='Telefone para contato', default='')
+    cep = models.CharField('CEP', max_length=8, default='')
+    numeroCasa = models.IntegerField('Número', default='')
+    rua = models.CharField('Rua', max_length=100, default='')
+    bairro = models.CharField('Bairro', max_length=100, default='')
+    cidade = models.CharField('Cidade', max_length=100, default='')
+    estado = models.CharField('Estado', max_length=5, choices=[(tag, tag.value) for tag in BRStates])
+    complemento = models.CharField('Complemento', max_length=100, default='')
 
     dataCriacao = models.DateTimeField('Criado em', auto_now_add=True)
     dataModificacao = models.DateTimeField('Última modificação', auto_now=True)
@@ -55,8 +97,19 @@ class Manufacturer(models.Model):
 class Affiliated(models.Model):
     slug = models.SlugField('Identificador', max_length=100)
     nome = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, default='')
+    dataNascimento = models.DateField('Data de Nascimento', default='')
     email = models.EmailField('E-mail', max_length=40, unique= True)
+    emailConfirmado = models.BooleanField('E-mail confirmado', default=False)
     senha = models.CharField('Senha', max_length=30)
+    telefone = PhoneField('Telefone', help_text='Telefone para contato', default='')
+    cep = models.CharField('CEP', max_length=8, default='')
+    numeroCasa = models.IntegerField('Número', default='')
+    rua = models.CharField('Rua', max_length=100, default='')
+    bairro = models.CharField('Bairro', max_length=100, default='')
+    cidade = models.CharField('Cidade', max_length=100, default='')
+    estado = models.CharField('Estado', max_length=5, choices=[(tag, tag.value) for tag in BRStates])
+    complemento = models.CharField('Complemento', max_length=100, default='')
 
     dataCriacao = models.DateTimeField('Criado em', auto_now_add=True)
     dataModificacao = models.DateTimeField('Última modificação', auto_now=True)
