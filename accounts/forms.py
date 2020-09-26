7836c2f377020203f301a29a9905d71331c48b75
Gabriel Lenon <glenonsilva@gmail.com>
2020-09-26 15:57:22 -0300

    first commit to github

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,4 @@ from accounts.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-<<<<<<< HEAD
         fields = ['nome', 'cpf', 'dataNascimento', 'email', 'senha', 'telefone', 'isProdutor']
-=======
-        fields = ['nome', 'cpf', 'dataNascimento', 'email', 'senha', 'telefone']
->>>>>>> 2de61abb52edc5456bba7a7913595b33133d95c9
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     path('registro/', views.userRegister, name='userRegister')
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 2de61abb52edc5456bba7a7913595b33133d95c9
